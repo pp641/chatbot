f9b3b4beecd737495ed1901f7f689be33097899d
pp641 <43077034+pp641@users.noreply.github.com>
2021-03-10 19:48:10 +0530

    Create Training_data.py

---
+++ Training_data.py
@@ -0,0 +1,18 @@
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+random.shuffle(training)
+training = np.array(training)
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data created")
